76018380edf3f46b8365d5b28b4b10494bbcabfb
Fernando <fernando.alvarez@uabc.edu.mx>
2021-05-22 13:25:17 -0700

    ⚡️ Cambia implementación a generar reportes por renglon de la tabla en data

--- src/csv_to_latex.py
+++ src/csv_to_latex.py
@@ -1,29 +1,34 @@
 import argparse
 import os
 import subprocess
+import pandas as pd
 
 content = r'''
 \newcommand\firstName{%(fname)s}
 \newcommand\lastName{%(lname)s}
 '''
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-fn', '--fname',) 
-parser.add_argument('-ln', '--lname',) 
+def check_for_error_cmd(cmd, return_code, file_name):
+    if not return_code == 0:
+        os.unlink(f'reports/{file_name}')
+        raise ValueError('Error {} executing command: {}'.format(return_code, ' '.join(cmd))) 
+
+def generate_pdf(file_name):
+    cmd = ['make', 'reports/template_format.pdf']
+    proc = subprocess.Popen(cmd)
+    proc.communicate()
+    return_code = proc.returncode
+    check_for_error_cmd(cmd, return_code, file_name)
 
 
-args = parser.parse_args()
+users_data = pd.read_csv('data/example_data.csv')
 
-with open('reports/info.tex','w') as f:
-    f.write(content%args.__dict__)
-
-cmd = ['make', 'reports/template_format.pdf']
-proc = subprocess.Popen(cmd)
-proc.communicate()
-
-retcode = proc.returncode
-if not retcode == 0:
-    os.unlink('reports/template_format.pdf')
-    raise ValueError('Error {} executing command: {}'.format(retcode, ' '.join(cmd))) 
-
-os.unlink('reports/info.tex')
+for _, row in users_data.iterrows():
+    otro_dict = {'fname': row["Nombre"], 'lname': row["Apellido"]}
+    file_name = "constancia_{}_{}.pdf".format(row["Nombre"], row["Apellido"])
+    with open('reports/info.tex'.format(),'w') as f:
+        f.write(content%otro_dict)
+    generate_pdf(file_name)
+    os.rename("reports/template_format.pdf",f"reports/{file_name}")
+    os.unlink('reports/info.tex')
+    
